6947799bc0aacf78f154aa97e9f9d1f9cd29b4e7
Fernando Camarena <fernando@camarenat.com>
2024-12-03 16:17:30 -0600

    Removing deprecated warning

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1733263446',resources: [
+    cacheName: 'chirpy-1733264148',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
