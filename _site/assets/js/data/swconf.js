7d5b59fb7d7a3776ff6228431ef692c84539369d
Fernando Camarena <fernando@camarenat.com>
2024-12-04 12:08:14 -0600

    alpha 3v

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1733272008',resources: [
+    cacheName: 'chirpy-1733335576',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
